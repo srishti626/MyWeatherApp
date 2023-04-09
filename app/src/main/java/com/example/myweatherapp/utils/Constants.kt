4d03f9a6b921e4310c0c89badec859dce9689187
srishti <63057124+srishti626@users.noreply.github.com>
2023-04-09 18:14:30 +0530

    Update Constants.kt

--- app/src/main/java/com/example/myweatherapp/utils/Constants.kt
+++ app/src/main/java/com/example/myweatherapp/utils/Constants.kt
@@ -11,17 +11,14 @@ object Constants {
     const val BASE_URL: String = "http://api.openweathermap.org/data/"
     const val METRIC_UNIT: String = "metric"
 
-    // TODO (STEP 3: Add the SharedPreferences name and key name for storing the response data in it.)
-    // START
+   
     const val PREFERENCE_NAME = "WeatherAppPreference"
     const val WEATHER_RESPONSE_DATA = "weather_response_data"
-    // END
 
-    /**
-     * This function is used check the weather the device is connected to the Internet or not.
-     */
+
+   
     fun isNetworkAvailable(context: Context): Boolean {
-        // It answers the queries about the state of network connectivity.
+    
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val network      = connectivityManager.activeNetwork ?: return false
@@ -29,12 +26,12 @@ object Constants {
             return when {
                 activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
                 activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                //for other device how are able to connect with Ethernet
+              
                 activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
                 else -> false
             }
         } else {
-            // Returns details about the currently active default data network.
+          
             val networkInfo = connectivityManager.activeNetworkInfo
             return networkInfo != null && networkInfo.isConnectedOrConnecting
         }
